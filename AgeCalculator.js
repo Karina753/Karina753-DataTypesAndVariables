e72fa2853a936eadcaee2d181c90c3cd425fb180
Karina753 <karinasanchez.753@gmail.com>
2021-11-12 14:37:59 -0600

    DataTypesAndVariables

---
+++ AgeCalculator.js
@@ -0,0 +1,16 @@
+// CALCULADORA 
+let  mesActual= { mes:"09"};
+let  cumple = {mes: "06", año: "1994"};
+let añoFuturo= 2050;
+
+function calcular(){
+let edad = añoFuturo - cumple.año;
+num =edad - 1;
+  if (cumple.mes <= 12, mesActual.mes){
+    console.log("La edad es:", edad);
+  }else{
+    console.log("La edad es :", num);
+  } 
+
+}
+calcular();
---
+++ CardInformation.js
@@ -0,0 +1,7 @@
+
+let nombre = "karina";
+let ciudad = "Guadalajara";
+let gustos = "comer";
+
+
+console.log("Hola¡, mi nombre es  ", nombre, " actualmente vivo en " , ciudad , " y me gusta  mucho ", gustos);
